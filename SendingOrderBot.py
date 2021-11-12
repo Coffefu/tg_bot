c53471a4c0752c6f5c4f29593bbd5567359a0862
Andrey <prosin270901@gmail.com>
2021-11-12 15:55:28 +1000

    boot_waiter_cmd file as a class

---
+++ SendingOrderBot.py
@@ -0,0 +1,32 @@
+import json
+import telebot
+from order import Order
+from dotenv import dotenv_values
+
+
+class SendingOrderBot:
+    order: Order
+    bot: telebot.TeleBot
+
+    def __init__(self, order: Order):
+        self.order = order
+        self.bot = telebot.TeleBot(dotenv_values(".env")['TOKEN'], parse_mode=None)
+
+    def __str__(self):
+        message = f'<b>Заказ №{self.order.order_number}</b>\n'
+        message += f'<i>Содержание:</i> {self.order.order_content}\n'
+        message += f'<i>Приготовить к:</i> {str(self.order.time.time())[:5]}\n'  # fixme
+        if self.order.phone_number:
+            message += f'<i>Телефон покупателя:</i> +7{self.order.phone_number}\n'
+        if self.order.email:
+            message += f'<i>Почта покупателя:</i> {self.order.email}\n'
+
+        return message
+
+    def send_message(self):
+        with open('baristas.txt', 'r') as file:
+            baristas = json.loads(file.read())
+
+        if self.order.coffee_house_id in baristas:
+            for i in baristas[self.order.coffee_house_id]:
+                self.bot.send_message(chat_id=i, text=str(self), parse_mode='HTML')
